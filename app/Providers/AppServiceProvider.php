4ed1ffe06afd190d348bccc7defa17cb5ddd5aa7
rgm94 <rgmartos1994@gmail.com>
2026-06-15 08:42:29 +0200

    fix: forzar esquema HTTPS en entornos de produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,9 +17,12 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
+     * Si la variable APP_ENV en Render es 'production', forzamos HTTPS en todos los enlaces y formularios
      */
     public function boot(): void
     {
-        //
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
